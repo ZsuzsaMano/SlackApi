976f49bf3c55ff0eabd5dfea0332b2637ca6c880
Zsuzsa <manoaway@gmail.com>
2021-03-12 11:38:04 +0100

    replace in schema with variables in createUsers

--- createUsers.js
+++ createUsers.js
@@ -1,17 +1,28 @@
 const axios = require("axios");
-const users = require("./testusers.json");
+const users = require("./JSONfiles/testusers0.json");
 const token = require("./config.js");
 
 const createUsers = () => {
   users.forEach(user => {
-    let data =
-      '{\n    "schemas": [\n        "urn:scim:schemas:core:1.0",\n     "urn:scim:schemas:extension:enterprise:1.0"\n    ],\n    "userName": "test25_username25",\n    "name": {\n        "familyName": "Last",\n   "givenName": "First",\n    },\n    "emails": [\n {\n   "value": "testh5@email.com",\n       "type": "work",\n            "primary": true\n        },\n     \n    ],\n}\n';
+    let data = JSON.stringify({
+      schemas: [
+        "urn:scim:schemas:core:1.0",
+        "urn:scim:schemas:extension:enterprise:1.0"
+      ],
+      userName:
+        user.FirstName.toLowerCase() + "_" + user.LastName.toLowerCase(),
+      name: { familyName: user.FirstName, givenName: user.LastName },
+      displayName: user.FirstName + " " + user.LastName,
+      emails: [{ value: user.EmailAddress, type: "work", primary: true }]
+    });
+
+    console.log(data);
 
     const config = {
       method: "post",
       url: "https://api.slack.com/scim/v1/Users",
       headers: {
-        Authorization: "Bearer " + token.botToken,
+        Authorization: "Bearer " + token.userToken,
         "Content-Type": "application/json",
         Cookie: "b=68cdyyggh2sswgno3eq3se4em"
       },
