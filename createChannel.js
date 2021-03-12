80e27bb22def702a88b91dce8b5ea3c1c5d277b9
Zsuzsa <manoaway@gmail.com>
2021-03-12 11:36:08 +0100

    change require pass

--- createChannel.js
+++ createChannel.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
 const qs = require("qs");
-const groups = require("./testgroups.json");
+const groups = require("./JSONfiles/testgroups.json");
 const fs = require("fs");
 const token = require("./config.js");
+const writeJson = require("./common/writeJson.js");
 
 const jsonLenght = groups.length;
 
@@ -27,19 +28,12 @@ const createChannel = () => {
 
     axios(config)
       .then(function(response) {
-        const groupId = JSON.stringify(response.data.channel.id);
-        console.log(groupId);
+        const groupId = response.data.channel.id;
         groupArray.push({ groupId: groupId, GroupName: group.group });
         if (groupArray.length === jsonLenght) {
           const toFile = JSON.stringify(groupArray);
 
-          fs.writeFile("./groupId.json", toFile, err => {
-            if (err) console.log(err);
-            else {
-              console.log("File written successfully\n");
-              console.log(fs.readFileSync("./groupId.json", "utf8"));
-            }
-          });
+          writeJson(groupId, toFile);
         }
       })
       .catch(function(error) {
