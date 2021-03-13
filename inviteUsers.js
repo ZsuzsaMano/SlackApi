531b83ac826389f67fc45d31d1fa30f973d60ddc
Zsuzsa <manoaway@gmail.com>
2021-03-13 23:38:11 +0100

    invite from mergedData

--- inviteUsers.js
+++ inviteUsers.js
@@ -1,26 +1,33 @@
 const axios = require("axios");
 const qs = require("qs");
 const token = require("./config.js");
+const mergedData = require("./JSONfiles/mergedData.json");
 
-const data = qs.stringify({
-  token: token.userToken,
-  channel: "C01RMAH9LAC",
-  users: "U01QQTLAUAZ,U01QQTFR5LM"
-});
-const config = {
-  method: "post",
-  url: "https://slack.com/api/conversations.invite",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    Cookie: "b=68cdyyggh2sswgno3eq3se4em"
-  },
-  data: data
+const inviteUsers = () => {
+  mergedData.forEach(mData => {
+    const data = qs.stringify({
+      token: token.userToken,
+      channel: mData.groupId,
+      users: mData.UserId
+    });
+    const config = {
+      method: "post",
+      url: "https://slack.com/api/conversations.invite",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Cookie: "b=68cdyyggh2sswgno3eq3se4em"
+      },
+      data: data
+    };
+
+    axios(config)
+      .then(function(response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  });
 };
 
-axios(config)
-  .then(function(response) {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+inviteUsers();
