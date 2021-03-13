eeeb4cbd9250306dbd4262071f6b36ae40f35508
Zsuzsa <manoaway@gmail.com>
2021-03-13 21:21:24 +0100

    merge and write returned data

---
+++ mergeReturnedData.js
@@ -0,0 +1,12 @@
+const mergeData = require("./common/mergeJson.js");
+const userId = require("./JSONfiles/userId.json");
+const groupId = require("./JSONfiles/groupId.json");
+const writeJson = require("./common/writeJson.js");
+
+userId.map(x =>
+  Object.assign(
+    x,
+    groupId.find(y => y.commonkey === x.commonkey)
+  )
+);
+writeJson("mergedData", JSON.stringify(userId));
