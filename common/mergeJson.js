80cde9182d5dbb577d8d9c7785486f27cd578f91
Zsuzsa <manoaway@gmail.com>
2021-03-13 21:06:52 +0100

    write merged data to json

--- common/mergeJson.js
+++ common/mergeJson.js
@@ -1,8 +1,9 @@
-const res = (json1, json2, commonkey) => {
+exports.default = (json1, json2, commonkey) => {
   json2.map(x =>
     Object.assign(
       x,
       json1.find(y => y.commonkey == x.commonkey)
     )
   );
+  console.log(json2);
 };
