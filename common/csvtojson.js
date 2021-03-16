d159e38754e81943affb7293ce7eadd66010ba13
Zsuzsa <manoaway@gmail.com>
2021-03-16 14:52:35 +0100

    test

--- common/csvtojson.js
+++ common/csvtojson.js
@@ -1,14 +1,13 @@
 const CSVToJSON = require("csvtojson");
 const writeJson = require("./writeJson");
 
-module.exports = (csvfile, filename, callback) => {
+module.exports = (csvfile, filename) => {
   CSVToJSON()
     .fromFile(`./CSVfiles/${csvfile}.csv`)
     .then(items => {
       console.log(items);
       writeJson(filename, JSON.stringify(items));
     })
-    .then(callback())
     .catch(err => {
       // log error if any
       console.log(err);
