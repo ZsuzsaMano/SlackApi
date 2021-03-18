701e7a4368629b435898412d17e37b8b5ae34609
Zsuzsa <webdev.zsuzsa@gmail.com>
2021-03-18 11:51:13 +0100

    validation of user input

--- createUsers.js
+++ createUsers.js
@@ -9,62 +9,72 @@ const createUsers = () => {
 
   users.forEach((user, i) => {
     setTimeout(() => {
-      let data = JSON.stringify({
-        schemas: [
-          "urn:scim:schemas:core:1.0",
-          "urn:scim:schemas:extension:enterprise:1.0"
-        ],
-        userName:
-          user.FirstName.toLowerCase() +
-          "_" +
-          user.LastName.toLowerCase() +
-          Math.floor(Math.random() * 100),
-        name: { familyName: user.FirstName, givenName: user.LastName },
-        displayName: user.FirstName + " " + user.LastName,
-        emails: [
-          {
-            value: user.EmailAddress,
-            type: "work",
-            primary: true
-          }
-        ]
-      });
-
-      const config = {
-        method: "post",
-        url: "https://api.slack.com/scim/v1/Users",
-        headers: {
-          Authorization: "Bearer " + token.userToken,
-          "Content-Type": "application/json",
-          Cookie: "b=68cdyyggh2sswgno3eq3se4em"
-        },
-        data: data
-      };
-
-      axios(config)
-        .then(function(response) {
-          const userId = response.data.id;
-          const email = response.data.emails[0].value;
-          userArray.push({
-            UserId: userId,
-            email: email,
-            GroupName: user.GroupName,
-            Topic: user.Topic
-          });
-          const toFile = JSON.stringify(userArray);
-          writeJson("userId", toFile);
-        })
-        .catch(function(error) {
-          console.log(error);
-          userErrorArray.push({
-            error: error.message,
-            user: user.FirstName + " " + user.LastName,
-            email: user.EmailAddress,
-            GroupName: user.GroupName
-          });
-          const toErrorFile = JSON.stringify(userErrorArray);
-          writeJson("userError", toErrorFile);
+      if (
+        user.FirstName &&
+        user.LastName &&
+        user.EmailAddress &&
+        user.Topic &&
+        user.GroupName
+      ) {
+        let data = JSON.stringify({
+          schemas: [
+            "urn:scim:schemas:core:1.0",
+            "urn:scim:schemas:extension:enterprise:1.0"
+          ],
+          userName:
+            user.FirstName.toLowerCase() +
+            "_" +
+            user.LastName.toLowerCase() +
+            Math.floor(Math.random() * 100),
+          name: { familyName: user.FirstName, givenName: user.LastName },
+          displayName: user.FirstName + " " + user.LastName,
+          emails: [
+            {
+              value: user.EmailAddress,
+              type: "work",
+              primary: true
+            }
+          ]
         });
+
+        const config = {
+          method: "post",
+          url: "https://api.slack.com/scim/v1/Users",
+          headers: {
+            Authorization: "Bearer " + token.userToken,
+            "Content-Type": "application/json",
+            Cookie: "b=68cdyyggh2sswgno3eq3se4em"
+          },
+          data: data
+        };
+
+        axios(config)
+          .then(function(response) {
+            const userId = response.data.id;
+            const email = response.data.emails[0].value;
+            userArray.push({
+              UserId: userId,
+              email: email,
+              GroupName: user.GroupName,
+              Topic: user.Topic
+            });
+            const toFile = JSON.stringify(userArray);
+            writeJson("userId", toFile);
+          })
+          .catch(function(error) {
+            console.log(error);
+            userErrorArray.push({
+              error: error.message,
+              user: user.FirstName + " " + user.LastName,
+              email: user.EmailAddress,
+              GroupName: user.GroupName
+            });
+            const toErrorFile = JSON.stringify(userErrorArray);
+            writeJson("userError", toErrorFile);
+          });
+      } else {
+        console.log("error not all fields filled", user);
+      }
       console.log("timeout ");
     }, 340 * i);
   });
