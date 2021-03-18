d03af89dd67af75fc5f2fe505f47a256764e66e8
Zsuzsa <webdev.zsuzsa@gmail.com>
2021-03-18 15:32:07 +0100

    get user by mail

---
+++ getUser.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+const qs = require("qs");
+const token = require("./config.js");
+
+const getUser = email => {
+  const data = qs.stringify({
+    token: token.botToken,
+    email: email
+  });
+  var config = {
+    method: "post",
+    url: "https://slack.com/api/users.lookupByEmail",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Cookie: "b=y8jkoei1qsl1fwndue4t31uk"
+    },
+    data: data
+  };
+
+  axios(config)
+    .then(function(response) {
+      console.log(JSON.stringify(response.data.user.id));
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+};
