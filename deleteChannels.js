5149f7fea5c3e6d12893297876d7029ddcc0e2bb
Zsuzsa <webdev.zsuzsa@gmail.com>
2021-03-19 09:44:01 +0100

    loop delete channel

--- deleteChannels.js
+++ deleteChannels.js
@@ -2,24 +2,30 @@ var axios = require("axios");
 var qs = require("qs");
 const token = require("./config.js");
 
-var data = qs.stringify({
-  token: token.userToken,
-  channel: "C01S2NJSREV"
-});
-var config = {
-  method: "post",
-  url: "https://slack.com/api/conversations.archive",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    Cookie: "b=y8jkoei1qsl1fwndue4t31uk"
-  },
-  data: data
-};
+const deleteChannel = inputArray => {
+  inputArray.forEach((group, i) => {
+    setTimeout(() => {
+      var data = qs.stringify({
+        token: token.userToken,
+        channel: inputArray.groupId
+      });
+      var config = {
+        method: "post",
+        url: "https://slack.com/api/conversations.archive",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Cookie: "b=y8jkoei1qsl1fwndue4t31uk"
+        },
+        data: data
+      };
 
-axios(config)
-  .then(function(response) {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function(error) {
-    console.log(error);
+      axios(config)
+        .then(function(response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }, 3000 * i);
   });
+};
