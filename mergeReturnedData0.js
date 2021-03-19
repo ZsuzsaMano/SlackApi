05a0305f2d831229e6c708a827a4f3e2b05ec7e5
Zsuzsa <webdev.zsuzsa@gmail.com>
2021-03-19 17:03:46 +0100

    fast fix?

--- mergeReturnedData0.js
+++ mergeReturnedData0.js
@@ -2,7 +2,7 @@ const userId = require("./JSONfiles/userId.json");
 
 const writeJson = require("./common/writeJson.js");
 
-const defaultGroupId = require("./JSONfiles/defaultGroupId.json")
+const defaultGroupId = require("./JSONfiles/defaultGroupId.json");
 // groupId.map(x => {
 //   userId.forEach(y => {
 //     if (x.GroupName === y.GroupName) {
@@ -22,56 +22,58 @@ const defaultGroupId = require("./JSONfiles/defaultGroupId.json")
 //   });
 // });
 
-const batchSize = 900;
-
-let batches = {}
-let i = 0
-let users = userId.map( u => u.UserId )
-
-for ( let batch of [1,2,3,4] ) {
-
-
-  const newBatch = users.splice(0, batchSize)
-  users = users.filter( (a) => !newBatch.includes(a) )
-  console.log({ batch, users: users, newBatch: newBatch.length })
-  batches[batch] = newBatch
-  // if (batches[batch]) {
-  //   batches[batch].push(newBatch)
-  // } else {
-  //   batches[batch] = [newBatch]
-  // }
-
-}
-
-console.log(JSON.stringify(batches))
-writeJson("userBatches", JSON.stringify(batches));
-
-const groups = []
-
-// foreach batch
-for (let batch of [1,2,3,4] ) {
-
-  // foreach group
-  for (let group of defaultGroupId) {
-    console.log(group)
-    group.users = batches[batch]
-    groups.push(group)
-  }
-}
+// const batchSize = 900;
+//
+// let batches = {}
+// let i = 0
+// let users = userId.map( u => u.UserId )
+//
+// for ( let batch of [1,2,3,4] ) {
 //
 //
-// //
-// // defaultGroupId.map(x => {
-// //   userId.forEach(y => {
-// //     x.users.push(y.UserId);
-// //   });
-// // });
+//   const newBatch = users.splice(0, batchSize)
+//   users = users.filter( (a) => !newBatch.includes(a) )
+//   console.log({ batch, users: users, newBatch: newBatch.length })
+//   batches[batch] = newBatch
+// if (batches[batch]) {
+//   batches[batch].push(newBatch)
+// } else {
+//   batches[batch] = [newBatch]
+// }
 //
-// // const allIds = userId.map(x => {
-// //     x.users.push(y.UserId);
-// // });
+// }
+//
+// console.log(JSON.stringify(batches))
+// writeJson("userBatches", JSON.stringify(batches));
+//
+// const groups = []
+//
+// // foreach batch
+// for (let batch of [1,2,3,4] ) {
+//
+//   // foreach group
+//   for (let group of defaultGroupId) {
+//     console.log(group)
+//     group.users = batches[batch]
+//     groups.push(group)
+//   }
+// }
+//
+//
+
+defaultGroupId.map(x => {
+  userId.forEach(y => {
+    x.users.push(y.UserId);
+  });
+});
+
+defaultGroupId.map(x => x.users.slice(0, 900));
+
+// const allIds = userId.map(x => {
+//     x.users.push(y.UserId);
+// });
 //
 //
 // console.log(defaultGroupId);
 // console.log(groups)
-writeJson("mergedDefaultData", JSON.stringify(groups));
+writeJson("mergedDefaultData", JSON.stringify(defaultGroupId));
